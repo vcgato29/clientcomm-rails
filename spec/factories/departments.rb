0fd6f11ee0b0fc7b1420fa8ff73a5b4d58c7c92b
Mark Rossetti <markrossetti@codeforamerica.org>
2017-12-18 13:40:35 -0800

    Added transfer note to transfer emails. Enabled analytics. Tested.
    [#153196288]
    
    Signed-off-by: Tomas Apodaca <tomas@codeforamerica.org>

--- spec/factories/departments.rb
+++ spec/factories/departments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :department do
-    name { Faker::HarryPotter.location }
+    name { Faker::HarryPotter.location.delete("'") }
     phone_number { "+1760555#{Faker::PhoneNumber.unique.subscriber_number}" }
   end
 end
